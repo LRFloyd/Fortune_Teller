4fb05700fcebc1b9743ce0a35a9f517dce4311ae
LRFloyd <lavelle.floyd@gmail.com>
2020-08-25 11:49:34 -0400

    first commit

---
+++ FortuneTeller/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //Welcome Message
+            Console.WriteLine("Welcome to Fortune Teller... a game where there is a 99.9% chance most of this fortune WONT come true.");
+            Console.WriteLine("At any time you can type 'Help' for additional information, Lets BEGIN");
+            Console.WriteLine("Please provide me some information about you.");
+
+
+
+            string firstName, lastName, retire;
+            string color = "";
+            string check = "";
+            string home = "";
+            string money = "";
+            string transport = "";
+            int bMonth, siblings;
+            int summ = 0;
+            int age = 0;
+            int remainingWork = 75;
+            
+            
+        }
+    }
+}
+        
